200ced3cb60c72f434f8acd161c7d8dbd855d275
Adhithyan H Nair <133254493+adhithyan-h-nair@users.noreply.github.com>
2025-10-24 06:46:23 +0530

    Add files via upload

---
+++ lex analyzer.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// Function to check if a string is a keyword
+int isKeyword(char buffer[]) {
+    char keywords[32][10] = {"auto", "break", "case", "char", "const", "continue", "default",
+                             "do", "double", "else", "enum", "extern", "float", "for", "goto",
+                             "if", "int", "long", "register", "return", "short", "signed",
+                             "sizeof", "static", "struct", "switch", "typedef", "union",
+                             "unsigned", "void", "volatile", "while"};
+    int i, flag = 0;
+    for (i = 0; i < 32; ++i) {
+        if (strcmp(keywords[i], buffer) == 0) {
+            flag = 1;
+            break;
+        }
+    }
+    return flag;
+}
+
+int main() {
+    char ch, buffer[15], operators[] = "+-*/%=";
+    FILE *fp;
+    int i, j = 0;
+
+    fp = fopen("input.txt", "r");
+
+    if (fp == NULL) {
+        printf("Error while opening the file\n");
+        return 0;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        for (i = 0; i < 6; ++i) {
+            if (ch == operators[i])
+                printf("%c is an operator\n", ch);
+        }
+
+        if (isalnum(ch)) {
+            buffer[j++] = ch;
+        } else if ((ch == ' ' || ch == '\n') && (j != 0)) {
+            buffer[j] = '\0';
+            j = 0;
+
+            if (isKeyword(buffer) == 1)
+                printf("%s is a keyword\n", buffer);
+            else
+                printf("%s is an identifier\n", buffer);
+        }
+    }
+
+    fclose(fp);
+    return 0;
+}
